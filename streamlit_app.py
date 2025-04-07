b8f3514a78333708aa69294bc8ceff54b2ae8937
Md. Khaled Hossain <41100729+khaledcs@users.noreply.github.com>
2025-04-06 21:05:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to Mr Khaled [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, head over to Mr Khaled ha ha [docs.streamlit.io](https://docs.streamlit.io/)."
 )
